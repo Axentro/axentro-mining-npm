ee846ef560fe7bf705d37864802027e526f34692
Kingsley <kingsleyhendrickse@me.com>
2020-12-10 20:09:18 +0000

    async stuff working now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cluster = require('cluster');
 
-const mining = function(url, address, nprocesses) {
+const mining = async function(url, address, nprocesses) {
     if (cluster.isMaster) {
 	const client = require('./lib/client');
 	client.connect(url, address, nprocesses);
