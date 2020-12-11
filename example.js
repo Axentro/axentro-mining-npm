e9c38c266d6610dd780391912b01a8a48694936c
Kingsley <kingsleyhendrickse@me.com>
2020-12-11 22:41:44 +0000

    yay working

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ const mining = require("./index");
 
 const url = 'http://localhost:3000/peer';
 const address = 'VDAwZTdkZGNjYjg1NDA1ZjdhYzk1M2ExMDAzNmY5MjUyYjI0MmMwNGJjZWY4NjA3';
-const nprocesses = 1;
+const nprocesses = 2;
 
 mining(url, address, nprocesses);
