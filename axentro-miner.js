089fde563231979c7a7bf55927c8422a99738e5b
Kingsley <kingsleyhendrickse@me.com>
2020-12-16 06:54:35 +0000

    fixed problem with miner dying on update

--- axentro-miner.js
+++ axentro-miner.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const mining = require("./index");
-const { program } = require('commander');
-program.version('0.0.2');
+const {
+    program
+} = require('commander');
+program.version('0.0.3');
 
 program
-  .requiredOption('-a, --address <address>', 'Your wallet address')
-  .option('-n, --node <node>', 'Node url to mine against', 'http://mainnet.axentro.io')
-  .option('-p, --process <process>', 'Number of processes to use', 1);
+    .requiredOption('-a, --address <address>', 'Your wallet address')
+    .option('-n, --node <node>', 'Node url to mine against', 'http://mainnet.axentro.io')
+    .option('-p, --process <process>', 'Number of processes to use', 1);
 
-  program.parse(process.argv);
+program.parse(process.argv);
 
 clear();
 
 console.log(
-  chalk.green(
-    figlet.textSync('Axentro', { horizontalLayout: 'full' })
-  ));
+    chalk.green(
+        figlet.textSync('Axentro', {
+            horizontalLayout: 'full'
+        })
+    ));
 
- 
+
 console.log(chalk.green('Node: ') + chalk.cyan(program.node));
 console.log(chalk.green('Process count: ') + chalk.cyan(program.process));
 console.log(chalk.green('Address: ') + chalk.cyan(program.address));
 console.log('');
-  
+
 mining(program.node, program.address, program.process);
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const cluster = require('cluster');
 
 const mining = async function(url, address, nprocesses) {
     if (cluster.isMaster) {
-	const client = require('./lib/client');
-	client.connect(url, address, nprocesses);
+        const client = require('./lib/client');
+        client.connect(url, address, nprocesses);
     } else {
-	require('./lib/miner');
+        require('./lib/miner');
     }
 }
 
